4567d24a4dc9c6a38d86817a930b041ca609f7e1
Yiwei Huang <yiwei.huang@p46114.local>
2018-05-07 15:19:54 +0900

    lecture 236 finished

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Huang, Yiwei on 2018/05/07.
+//  Copyright © 2018 Huang, Yiwei. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["task1","task2","task3"]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //the first tableView is a global var
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Tableview Delegate Method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) //no gray out when tapped
+    }
+    
+   
+}
+
